b6d9548bd7c3025c072cf97e67f301d63cbb8216
Nikolay Spasov <niki.spasow@gmail.com>
2017-04-16 14:43:30 +0300

    User-role relation created

---
+++ models/Role.js
@@ -0,0 +1,12 @@
+/**
+ * Created by macintoshhd on 4/16/17.
+ */
+const mongoose = require('mongoose');
+
+let roleSchema = mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+});
+const Role = mongoose.model('Role', roleSchema);
+
+module.exports = Role;
