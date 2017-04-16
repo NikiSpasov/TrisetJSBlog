c4fdb6b0929116d4554378030ce6b2174bf1d993
Nikolay Spasov <niki.spasow@gmail.com>
2017-04-16 13:38:04 +0300

    Basic blog function are now working!
    "Details" added, views updated, controllers updated, routes.js updated,
    now you can to create articles, read more functionality, etc.

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,14 @@
 const userController = require('./../controllers/user');
 const homeController = require('./../controllers/home');
+const articleController = require('./../controllers/article');
 
 module.exports = (app) => {
     app.get('/', homeController.index);
+
+    app.get('/article/create', articleController.createGet);
+    app.post('/article/create', articleController.createPost);
+
+    app.get('/article/details/:id', articleController.details);
 
     app.get('/user/register', userController.registerGet);
     app.post('/user/register', userController.registerPost);
--- controllers/article.js
+++ controllers/article.js
@@ -12,24 +12,24 @@ module.exports = {
         let articleArgs = req.body;
         let errorMsg = '';
 
-        if(!req.isAuthenticated()) {
+        if (!req.isAuthenticated()) {
             errorMsg = 'First log-in, then write your article!';
         }
-        else if(!articleArgs.title){
+        else if (!articleArgs.title) {
             errorMsg = 'Name your article, please!';
         }
         else if (!articleArgs.content) {
             errorMsg = 'Dude, write somethin\' first!';
         }
         if (errorMsg) {
-            res.render ('article/create', {error: errorMsg});
+            res.render('article/create', {error: errorMsg});
             return;
         }
         articleArgs.author = req.user.id;
-        Article.create(articleArgs).then(article=> {
+        Article.create(articleArgs).then(article => {
             req.user.articles.push(article.id);
             req.user.save(err => {
-                if (err){
+                if (err) {
                     res.redirect('/', {error: err.message});
                 }
                 else {
@@ -37,7 +37,12 @@ module.exports = {
                 }
             });
         })
+    },
+    details: (req, res) => {
+        let id = req.params.id;
+
+        Article.findById(id).populate('author').then(article => {
+            res.render('article/details', article)
+        });
     }
-
-
 };
