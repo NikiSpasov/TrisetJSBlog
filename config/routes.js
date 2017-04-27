6ecd15665cb3cd6f1bc4db85b0349f0a513a4db1
Nikolay Spasov <niki.spasow@gmail.com>
2017-04-27 23:58:21 +0300

    Added useerDetails view
    View added, minor changes in controllers and routes

--- config/routes.js
+++ config/routes.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
     app.get('/article/edit/:id', articleController.editGet);
     app.post('/article/edit/:id', articleController.editPost);
 
-    app.get('/article/delete/:id', articleController.deleteGet)
+    app.get('/article/delete/:id', articleController.deleteGet);
     app.post('/article/delete/:id', articleController.deletePost);
 
     app.get('/user/register', userController.registerGet);
